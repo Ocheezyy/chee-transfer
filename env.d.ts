cce4ce64597468b6c145dfced28436d8b8a0528c
ocheezyyy <seanodonnellse@gmail.com>
2024-11-16 02:17:57 -0500

    Added env.d.ts and updated tsconfig

---
+++ env.d.ts
@@ -0,0 +1,8 @@
+namespace NodeJS {
+    interface ProcessEnv {
+        SPOTIFY_CLIENT_ID: string;
+        SPOTIFY_CLIENT_SECRET: string;
+        APPLE_ID: string;
+        APPLE_SECRET: string;
+    }
+}

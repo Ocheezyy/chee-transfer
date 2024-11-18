8cf2a6c847fb7e6b18afed1cdfdbc4ad366e91f1
ocheezyyy <seanodonnellse@gmail.com>
2024-11-18 07:36:18 -0500

    Created function to get oauth token.

---
+++ app/client-functions/get-oauth-token.ts
@@ -0,0 +1,16 @@
+import { auth, clerkClient } from "@clerk/nextjs/server";
+
+const getOauthToken = async (provider: AccountProvider): Promise<string | null> => {
+    const { userId } = await auth();
+    if (!userId) return null;
+
+    const client = await clerkClient();
+    const clerkResponse = await client.users.getUserOauthAccessToken(
+        userId,
+        provider
+    );
+
+    const accessToken = clerkResponse.data[0]?.token;
+    if (!accessToken) return null;
+    return accessToken;
+};

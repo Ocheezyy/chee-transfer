6e73df82cac9db211db56faaed24eaad23cfda18
ocheezyyy <seanodonnellse@gmail.com>
2024-12-02 22:11:55 -0500

    Created starting schemas for database

--- db/schema.ts
+++ db/schema.ts
@@ -1,9 +1,31 @@
+import { sql } from "drizzle-orm";
 import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
 
-export const todos = sqliteTable("todos", {
+
+export const playlists = sqliteTable("playlists", {
     id: integer("id", {
-        mode: "number",
+        mode: "number"
     }).primaryKey({ autoIncrement: true }),
-    description: text("description").notNull(),
-    completed: integer("completed", { mode: "boolean" }).notNull().default(false),
+    name: text("name").notNull(),
+    platform: text("platform", { enum: ["apple", "spotify", "youtube"] }).notNull(),
+    extPlaylistId: text("extPlaylistId").notNull(),
+    songCount: integer("songCount").notNull(),
+});
+
+export const playlistTransfers = sqliteTable("playlistTransfers", {
+    id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    playlistId: integer("playlistId").references(() => playlists.id),
+    transferType: text("transferType", { enum: ["full", "update"] }).default("full").notNull(),
+    from: text("from", { enum: ["spotify", "apple", "youtube"] }).notNull(),
+    to: text("to", { enum: ["spotify", "apple", "youtube"] }).notNull(),
+    status: text("status", { enum: ["draft", "in_progress", "completed"] }).notNull(),
+    completedAt: integer("completedAt", { mode: "timestamp_ms" }),
+    createdAt: integer("createdAt", { mode: "timestamp_ms" }).default(sql`now()`).notNull(),
+});
+
+export const songs = sqliteTable("songs", {
+    id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    isrc: text("isrc").notNull(),
+    status: text("status", { enum: ["success", "conflict", "failed"] }),
+    createdAt: integer("createdAt", { mode: "timestamp_ms" }).default(sql`now()`).notNull(),
 });
